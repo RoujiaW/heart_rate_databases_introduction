28ccc97ad1a99bfcc553d9afc8abdd3dc69b3224
yz398 <35477789+yz398@users.noreply.github.com>
2018-03-21 12:35:42 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,11 +4,11 @@ import models
 import datetime
 import m1
 app = Flask(__name__)
-
+connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
 @app.route("/api/heart_rate", methods=["POST"])
 def store():
 	r = request.get_json()
-	connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
+#	connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
 	create_user(email=r["user_email"], age=r["user_age"], heart_rate=r["heart_rate"])
 	add_heart_rate(r["user_email"], r["user_age"], datetime.datetime.now())
 #	print_user(r["user_email"])
