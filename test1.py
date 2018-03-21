f31ae8bc38228445d5281c060cd0b696d4d6c45e
yz398 <35477789+yz398@users.noreply.github.com>
2018-03-21 13:03:33 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,8 +9,8 @@ connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
 def store():
 	r = request.get_json()
 #	connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
-	m1.create_user(email=r["user_email"], age=r["user_age"], heart_rate=r["heart_rate"])
-	m1.add_heart_rate(r["user_email"], r["user_age"], datetime.datetime.now())
+	create_user(email=r["user_email"], age=r["user_age"], heart_rate=r["heart_rate"])
+	add_heart_rate(r["user_email"], r["user_age"], datetime.datetime.now())
 #	print_user(r["user_email"])
 	info = r["user_email"]
 	return info, 200
