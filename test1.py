4618795719d20e42a13177f3589726371524d13a
VCM admin account <vcm@vcm-3551.vm.duke.edu>
2018-03-21 12:28:23 -0400

    test

---
+++ test1.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request 
+from pymodm import connect
+import models
+import datetime
+import m1
+app = Flask(__name__)
+
+@app.route("/api/heart_rate", methods=["POST"])
+def store():
+	r = request.get_json()
+	connect("mongodb://vcm-3551.vm.duke.edu:27017/heart_rate_app")
+	create_user(email=r["user_email"], age=r["user_age"], heart_rate=r["heart_rate"])
+	add_heart_rate(r["user_email"], r["user_age"], datetime.datetime.now())
+#	print_user(r["user_email"])
+	info = r["user_email"]
+	return info, 200
