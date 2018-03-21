b1b45b0b03ab3141166b927b84637e6592f6a2b3
yz398 <yz@DESKTOP-3IJBH7Q.localdomain>
2018-03-21 12:33:09 -0400

    change1

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, request
+#from flask import Flask, jsonify, request
 import requests
-from pymodm import connect
-import models
-import datetime
-import m1
+#from pymodm import connect
+#import models
+#import datetime
+#import m1
 
 r = requests.post("http://vcm-3551.vm.duke.edu:5000/api/heart_rate",json={"user_email": "suyash@suyashkumar.com",
     "user_age": 50, 
