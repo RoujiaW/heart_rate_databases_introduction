0d86078efc4ca7c636582889c0f28f0703fa249c
yz398 <yz@DESKTOP-3IJBH7Q.localdomain>
2018-03-22 15:34:42 -0400

    14d3

--- pro.py
+++ pro.py
@@ -69,7 +69,7 @@ def average_now():
 		}
 	else:
 		begin_location = user.heart_rate_times.index(time_begin)
-		for i in range(begin_location-1, len(user.heart_rate_times),1):
+		for i in range(begin_location, len(user.heart_rate_times),1):
 			sum_interval = user.heart_rate[i] + sum_interval
 	average_interval = sum_interval/(len(user.heart_rate_times)-begin_location+1)
 	case = {"average_interval": average_interval} 
