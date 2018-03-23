1c7c77a22df0f48853cb6b8be7b271e5c03bc5be
yz398 <yz@DESKTOP-3IJBH7Q.localdomain>
2018-03-22 23:36:00 -0400

    test

--- test_ta.py
+++ test_ta.py
@@ -2,6 +2,12 @@ def test_ta():
 	"""
 	test ta function which return whether this person is normal or not
 	"""	
+	try:
+		import pytest
+		import ta
+	except ImportError:
+		print("Necessary imports for this test function failed")
+		return
 	age1 = 70
 	heart_rate1 = 150
 	test_answer1 = 1
